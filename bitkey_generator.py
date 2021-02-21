58cf6e3d783a92e5d6017e8924aff5f1b437171e
Tyton25 <tybenz1gmail.com>
2021-02-21 16:25:19 -0700

    Adds bitcoin_key_gen.py

--- bitkey_generator.py
+++ bitkey_generator.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+
+'''
+Creates BitCoin Wallet complaint credentials:
+- Public Key
+- Private Key
+- Private Key (Wallet Import Format)
+'''
+
 import os
 import ecdsa
 import binascii
@@ -8,7 +17,7 @@ import base58
 def main():
     # private_key = os.urandom(32).encode('hex')
     private_key = binascii.hexlify(os.urandom(32)).decode()
-    print("private_key: {}".format(private_key))
+    print("private_key: {}".format(type(private_key)))
 
     Private_Key = bytes.fromhex(private_key)
     print("Private_Key: ".format(Private_Key))
@@ -22,7 +31,9 @@ def main():
     print("public key = " + public_key.hex())
 
     compress_pubkey = False
-    pubkey = public_key
+    pubkey = public_key.hex()
+    # pubkey = "04f25c3fbbf53bb811f1ad9654a9bce8b79f34dae5618ef83e1abc52911970f04c58d10d79e1cdac2b8c59dc8fc2be5ed62f7a01726bc923c8c68cea818366da13"
+    print("pubkey: ".format(pubkey))
 
     if compress_pubkey:
         if ord(bytearray.fromhex(pubkey[-2:])) % 2 == 0:
@@ -35,11 +46,9 @@ def main():
         hex_str = bytearray.fromhex(pubkey)
 
     # Obtain key:
-
     key_hash = '00' + hash160(hex_str)
 
     # Obtain signature:
-
     sha = hashlib.sha256()
     sha.update( bytearray.fromhex(key_hash) )
     checksum = sha.digest()
@@ -47,16 +56,18 @@ def main():
     sha.update(checksum)
     checksum = sha.hexdigest()[0:8]
 
-    print("checksum = \t" + sha.hexdigest() )
-    print("key_hash + checksum = \t" + key_hash + ' ' + checksum)
-    print("bitcoin address = \t" + base58.b58encode(bytes(bytearray.fromhex(key_hash + checksum))))
+    print("checksum = \t{}".format(sha.hexdigest()))
+    print("key_hash + checksum = \t{}".format(key_hash + ' ' + checksum))
+    key_cksum = str(key_hash + checksum)
+    print("key_cksum: ".format(key_cksum))
+    print("bitcoin address = \t" + base58.b58encode(bytes(bytearray.fromhex(key_cksum))))
 
 
 def hash160(hex_str):
     sha = hashlib.sha256()
     rip = hashlib.new('ripemd160')
     sha.update(hex_str)
-    rip.update(sha.digest() )
+    rip.update(sha.digest())
     print("key_hash = \t" + rip.hexdigest())
     return rip.hexdigest()  # .hexdigest() is hex ASCII
 
